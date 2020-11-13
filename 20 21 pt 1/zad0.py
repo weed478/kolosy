9b3a7e169a783b0b9b17b761e75385deba1b442e
Carbon <carbon.szlachta@gmail.com>
2020-11-13 21:27:43 +0100

    test2

--- 20 21 pt 1/zad0.py
+++ 20 21 pt 1/zad0.py
@@ -1,21 +1,41 @@
+import math
+
+
+def check(n, digit):
+    fact = math.factorial(n)
+    while fact % 10 == 0:
+        fact //= 10
+
+    return fact % 10 == digit
+
+
 def last_factorial_digit(n):
     factorial = 1
 
     for i in range(1, n + 1):
-        factorial *= i
+        while i % 10 == 0:
+            i //= 10
+
         while factorial % 10 == 0:
             factorial //= 10
 
-        what = 10
-        while ((factorial % what) * (i + 1)) % what == 0:
-            what *= 10
+        digit_mask = 10
+        while ((factorial % digit_mask) * (i % digit_mask)) % digit_mask == 0:
+            digit_mask *= 10
 
-        factorial = factorial % (what * 10)
+        factorial %= digit_mask
+        i %= digit_mask
+        factorial *= i
 
-        print("f({}) = {}".format(i, factorial))
+        # print("f({}) = {}".format(i, factorial))
+
+    while factorial % 10 == 0:
+        factorial //= 10
 
     return factorial % 10
 
 
-N = 30000
-print("f({}) = {}".format(N, last_factorial_digit(N)))
+for N in range(200):
+    last_digit = last_factorial_digit(N)
+    if not check(N, last_digit):
+        print("f({}) = {}".format(N, last_digit))
